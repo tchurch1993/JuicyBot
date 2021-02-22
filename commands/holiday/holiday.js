3f25a4bb6d1e777b6e1e24cac7fd2a5c77e0fc2f
Tyler Church <tchurch1993@gmail.com>
2021-02-22 00:49:16 -0500

    halfway fixed the holiday command

--- commands/holiday/holiday.js
+++ commands/holiday/holiday.js
@@ -34,13 +34,13 @@ class HolidayCommand extends commando.Command {
             var selector = cheerio.load(html);
             //console.log(html);
 
-            var HolidayElement = selector("body").find("div[class='card card--day card--alt linked']").first();
+            var HolidayElement = selector("body").find("div[class='card card--day linked']").first();
             var holidayImageElement = HolidayElement.find("div[class='card__media card__image cover']").find("img");
             var holidayImageLink = holidayImageElement[0].attribs.src;
 
             var holidayNameElement = HolidayElement.find("div[class='card__content']");
             var holidayTitle = holidayNameElement.find("h3[class='card__title heading']").text();
-            var holidayURL = holidayNameElement.find("h3[class='card__title heading']").find("a[class=js-link-target]")[0].attribs.href
+            var holidayURL = holidayNameElement.find("h3[class='card__title heading']").find("a[class=js-link-target]")[0].attribs.href;
             var holidayDescription = holidayNameElement.find("div[class='card__excerpt']").text();
 
             var holidayMessage = new discord.MessageEmbed()
@@ -55,6 +55,7 @@ class HolidayCommand extends commando.Command {
 
         } catch (err) {
             console.error(err);
+
 
         }
 
